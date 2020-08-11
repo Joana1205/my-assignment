7b209c1b78285ffa145dd7298871fd3a2e87b7a0
Joana1205 <67370913+Joana1205@users.noreply.github.com>
2020-08-11 10:30:19 +0100

    react assignment

---
+++ precache-manifest.c221562f89dc0db4367e1beaf10091d5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5b7d9c5ad646912a722e7f501f761ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc6464e22210f4855a1a",
+    "url": "/static/css/main.bfc225ea.chunk.css"
+  },
+  {
+    "revision": "090d94a4f1c71e0b427f",
+    "url": "/static/js/2.57992f05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.57992f05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc6464e22210f4855a1a",
+    "url": "/static/js/main.ef9f6559.chunk.js"
+  },
+  {
+    "revision": "eeaf9e17c28868ef20ae",
+    "url": "/static/js/runtime-main.2c9eb35c.js"
+  },
+  {
+    "revision": "5f1cd582c6d4fb5f4d86e6c1635deec2",
+    "url": "/static/media/hulk-1.5f1cd582.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "294dd39b7ab19aedb004eb8f359c307f",
+    "url": "/static/media/venom-1.294dd39b.jpg"
+  },
+  {
+    "revision": "5eb62fe8d11e8a32b2fb2554088f308e",
+    "url": "/static/media/venom-2.5eb62fe8.jpg"
+  }
+]);
